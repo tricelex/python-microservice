5b67417197d474fdd6fdf8eb8fb1bf7bd8d09f85
Chuckz Okoye <tricelex@gmail.com>
2021-01-03 14:03:49 +0100

    Added react components for react-crud service

--- react-crud/src/App.tsx
+++ react-crud/src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+import Menu from "./components/Menu";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+
+            <Nav/>
+
+            <div className="container-fluid">
+                <div className="row">
+                    <Menu/>
+
+                    <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+
+                    </main>
+                </div>
+            </div>
+
+
+        </div>
+);
 }
 
 export default App;
---
+++ react-crud/src/admin/Products.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Products = () => {
+    return (
+        <div className="table-responsive">
+            <table className="table table-striped table-sm">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Header</th>
+                    <th>Header</th>
+                    <th>Header</th>
+                    <th>Header</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>1,001</td>
+                    <td>Lorem</td>
+                    <td>ipsum</td>
+                    <td>dolor</td>
+                    <td>sit</td>
+                </tr>
+
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default Products;
