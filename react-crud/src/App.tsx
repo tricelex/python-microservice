3855a1b3935cc794e3774018380d338c53114ce9
Chuckz Okoye <tricelex@gmail.com>
2021-01-03 16:22:03 +0100

    Fixed components and added api calls to admin microservice

--- react-crud/src/App.tsx
+++ react-crud/src/App.tsx
@@ -1,27 +1,18 @@
 import React from 'react';
 import './App.css';
-import Nav from './components/Nav';
-import Menu from "./components/Menu";
+import {BrowserRouter, Route} from "react-router-dom";
+import Products from "./admin/Products";
+import Main from "./main/Main";
 
 function App() {
     return (
         <div className="App">
-
-            <Nav/>
-
-            <div className="container-fluid">
-                <div className="row">
-                    <Menu/>
-
-                    <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-
-                    </main>
-                </div>
-            </div>
-
-
+            <BrowserRouter>
+                <Route path='/' exact component={Main}/>
+                <Route path='/admin/products' component={Products}/>
+            </BrowserRouter>
         </div>
-);
+    );
 }
 
 export default App;
--- react-crud/src/admin/Products.tsx
+++ react-crud/src/admin/Products.tsx
@@ -1,30 +1,47 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import Wrapper from "./Wrapper";
 
 const Products = () => {
-    return (
-        <div className="table-responsive">
-            <table className="table table-striped table-sm">
-                <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>1,001</td>
-                    <td>Lorem</td>
-                    <td>ipsum</td>
-                    <td>dolor</td>
-                    <td>sit</td>
-                </tr>
 
-                </tbody>
-            </table>
-        </div>
+    useEffect(() => {
+        (
+            async () => {
+                const response = await fetch('http://localhost:8000/api/products');
+
+                const data = await response.json()
+
+                console.log(data)
+            }
+        )();
+    }, []);
+
+    return (
+        <Wrapper>
+            <div className="table-responsive">
+                <table className="table table-striped table-sm">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Header</th>
+                        <th>Header</th>
+                        <th>Header</th>
+                        <th>Header</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>1,001</td>
+                        <td>Lorem</td>
+                        <td>ipsum</td>
+                        <td>dolor</td>
+                        <td>sit</td>
+                    </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </Wrapper>
+
     );
 };
 
